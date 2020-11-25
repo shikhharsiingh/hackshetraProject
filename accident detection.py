bdd1a1cd935f9204fbcb293c5a1f1b41a974609a
Shikhhar Siingh <shikhharsiingh@Shikhhars-MacBook-Pro.local>
2020-11-25 23:22:35 +0530

    refined accident detection algo

--- accident detection.py
+++ accident detection.py
@@ -55,7 +55,10 @@ while res:
             if  w * h >10000:
                 area = w * h
                 #Countint the number of frames for which the condition persists to refine the accident detection case
-                
+                arcount += 1
+                #print(arcount)
+            if arcount > 35:
+                flag = 1
                 
     if flag == 1:
         #If accident detected print Accident on the screen
